3a6f75f6a6adf38a9762e84a2a7f8c4fc8ed1496
Bkacjios <blackops7799@gmail.com>
2020-06-16 15:35:50 -0400

    Use pointer addresses for Metroid Prime (Fixes #8)

--- source/modules/games/GM8E01-2.lua
+++ source/modules/games/GM8E01-2.lua
@@ -6,33 +6,37 @@ local game = {
 	memorymap = {}
 }
 
-local controllers = {
-	[1] = 0x807A6864 + 0xC * 0,
-	[2] = 0x807A6864 + 0xC * 1,
-	[3] = 0x807A6864 + 0xC * 2,
-	[4] = 0x807A6864 + 0xC * 3,
-}
+-- Pointer to a struct that contains the controller data
+local game_data_loc = 0x8045875C
 
 local controller_struct = {
-	[0x0] = { type = "short",	name = "controller.%d.buttons.pressed" },
-	[0x2] = { type = "sbyte",	name = "controller.%d.joystick.x" },
-	[0x3] = { type = "sbyte",	name = "controller.%d.joystick.y" },
-	[0x4] = { type = "sbyte",	name = "controller.%d.cstick.x" },
-	[0x5] = { type = "sbyte",	name = "controller.%d.cstick.y" },
-	[0x6] = { type = "byte",	name = "controller.%d.analog.l" },
-	[0x7] = { type = "byte",	name = "controller.%d.analog.r" },
-	[0xA] = { type = "byte",	name = "controller.%d.plugged" },
+	[0x0] = { type = "short",	name = "%d.buttons.pressed" },
+	[0x2] = { type = "sbyte",	name = "%d.joystick.x" },
+	[0x3] = { type = "sbyte",	name = "%d.joystick.y" },
+	[0x4] = { type = "sbyte",	name = "%d.cstick.x" },
+	[0x5] = { type = "sbyte",	name = "%d.cstick.y" },
+	[0x6] = { type = "byte",	name = "%d.analog.l" },
+	[0x7] = { type = "byte",	name = "%d.analog.r" },
+	[0xA] = { type = "byte",	name = "%d.plugged" },
 }
 
-for port, address in ipairs(controllers) do
+local controller_ptr = {
+	type = "pointer",
+	name = "controller",
+	struct = {}
+}
+
+for i=1,4 do
 	for offset, info in pairs(controller_struct) do
-		game.memorymap[address + offset] = {
+		controller_ptr.struct[0x04 + (0xC * (i-1)) + offset] = {
 			type = info.type,
 			debug = info.debug,
-			name = info.name:format(port),
+			name = info.name:format(i),
 		}
 	end
 end
+
+game.memorymap[game_data_loc] = controller_ptr
 
 game.translateAxis = core.translateAxis
 game.translateTriggers = core.translateTriggers
