110ff0df8a7cdb7007aedd4fb1dac2ca556a33ec
Bkacjios <blackops7799@gmail.com>
2020-07-01 15:54:06 -0400

    Map out slippi's match state struct Fix slippi-netplay setting not loading on startup

--- source/modules/games/GALE01-2.lua
+++ source/modules/games/GALE01-2.lua
@@ -4,6 +4,9 @@ local game = {
 	memorymap = {}
 }
 
+game.memorymap[0x80479D33] = { type = "byte", name = "menu" }
+game.memorymap[0x804807C8] = { type = "bool", name = "teams" }
+
 local controllers = {
 	[1] = 0x804C1FAC + 0x44 * 0,
 	[2] = 0x804C1FAC + 0x44 * 1,
@@ -26,6 +29,16 @@ local controller_struct = {
 	[0x34] = { type = "float",	name = "controller.%d.analog.r" },
 	[0x41] = { type = "byte",	name = "controller.%d.plugged" },
 }
+
+for port, address in ipairs(controllers) do
+	for offset, info in pairs(controller_struct) do
+		game.memorymap[address + offset] = {
+			type = info.type,
+			debug = info.debug,
+			name = info.name:format(port),
+		}
+	end
+end
 
 local player_static_addresses = {
 	0x80453080, -- Player 1
@@ -80,31 +93,32 @@ for id, address in ipairs(player_static_addresses) do
 	end
 end
 
-game.memorymap[0x80479D33] = { type = "byte", name = "menu" }
-game.memorymap[0x804807C8] = { type = "bool", name = "teams" }
+-- https://github.com/project-slippi/slippi-ssbm-asm/blob/6e08a376dc9ca239d9b7312089d975e89fa37a5c/Online/Online.s#L217
+local slippi_match_block = 0x80BDD440
 
-game.memorymap[0x80BDD44A] = {
-	type = "data",
-	len = 31,
-	name = "slippi.player.1.name",
-	debug = true
+-- 80005610
+-- 810F1600
+
+local slippi_match_state = {
+	[0x000] = { type = "byte", name = "slippi.connection_state" },
+	[0x001] = { type = "byte", name = "slippi.local_player.ready" },
+	[0x002] = { type = "byte", name = "slippi.remote_player.ready" },
+	[0x003] = { type = "byte", name = "slippi.local_player.index" },
+	[0x004] = { type = "byte", name = "slippi.remote_player.index" },
+	[0x005] = { type = "int", name = "slippi.rng_offset" },
+	[0x009] = { type = "byte", name = "slippi.delay_frames" },
+	[0x00A] = { type = "data", len = 31, name = "slippi.player.1.name" },
+	[0x029] = { type = "data", len = 31, name = "slippi.player.2.name" },
+	[0x048] = { type = "data", len = 31, name = "slippi.opponent_name" },
+	[0x067] = { type = "data", len = 241, name = "slippi.error_message" },
 }
 
-game.memorymap[0x80BDD469] = {
-	type = "data",
-	len = 31,
-	name = "slippi.player.2.name",
-	debug = true
-}
-
-for port, address in ipairs(controllers) do
-	for offset, info in pairs(controller_struct) do
-		game.memorymap[address + offset] = {
-			type = info.type,
-			debug = info.debug,
-			name = info.name:format(port),
-		}
-	end
+for offset, info in pairs(slippi_match_state) do
+	game.memorymap[slippi_match_block + offset] = {
+		type = info.type,
+		len = info.len,
+		name = info.name
+	}
 end
 
 function game.translateAxis(x, y)
