292b8a39321835e0f2756120dc09e1678b7f6237
Bkacjios <blackops7799@gmail.com>
2020-08-05 02:38:00 -0400

    Added support for Pikmin (NTSC v1.1) and Pikmin 2 (NTSC v1.0) (Issue #28)

---
+++ source/modules/games/GPVE01-0.lua
@@ -0,0 +1,40 @@
+-- Pikmin 2 (NTSC v1.0)
+
+local core = require("games.core")
+
+local game = {
+	memorymap = {}
+}
+
+local controllers = {
+	[1] = 0x80507008 + 0xC * 0,
+	[2] = 0x80507008 + 0xC * 1,
+	[3] = 0x80507008 + 0xC * 2,
+	[4] = 0x80507008 + 0xC * 3,
+}
+
+local controller_struct = {
+	[0x0] = { type = "short",	name = "controller.%d.buttons.pressed" },
+	[0x2] = { type = "sbyte",	name = "controller.%d.joystick.x" },
+	[0x3] = { type = "sbyte",	name = "controller.%d.joystick.y" },
+	[0x4] = { type = "sbyte",	name = "controller.%d.cstick.x" },
+	[0x5] = { type = "sbyte",	name = "controller.%d.cstick.y" },
+	[0x6] = { type = "byte",	name = "controller.%d.analog.l" },
+	[0x7] = { type = "byte",	name = "controller.%d.analog.r" },
+	[0xA] = { type = "byte",	name = "controller.%d.plugged" },
+}
+
+for port, address in ipairs(controllers) do
+	for offset, info in pairs(controller_struct) do
+		game.memorymap[address + offset] = {
+			type = info.type,
+			debug = info.debug,
+			name = info.name:format(port),
+		}
+	end
+end
+
+game.translateAxis = core.translateAxis
+game.translateTriggers = core.translateTriggers
+
+return game
