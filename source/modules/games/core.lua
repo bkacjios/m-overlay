b506d5b6f04ce37b77669d6a009c6be7af546c9c
Bkacjios <blackops7799@gmail.com>
2020-08-20 19:08:42 -0400

    Added function for loading a genric controller structure that most games seem to use.

--- source/modules/games/core.lua
+++ source/modules/games/core.lua
@@ -1,5 +1,35 @@
 local core = {}
 
+function core.loadGenericControllerMap(addr, memorymap)
+	local controllers = {
+		[1] = addr + 0xC * 0,
+		[2] = addr + 0xC * 1,
+		[3] = addr + 0xC * 2,
+		[4] = addr + 0xC * 3,
+	}
+
+	local controller_struct = {
+		[0x0] = { type = "short",	name = "controller.%d.buttons.pressed" },
+		[0x2] = { type = "sbyte",	name = "controller.%d.joystick.x" },
+		[0x3] = { type = "sbyte",	name = "controller.%d.joystick.y" },
+		[0x4] = { type = "sbyte",	name = "controller.%d.cstick.x" },
+		[0x5] = { type = "sbyte",	name = "controller.%d.cstick.y" },
+		[0x6] = { type = "byte",	name = "controller.%d.analog.l" },
+		[0x7] = { type = "byte",	name = "controller.%d.analog.r" },
+		[0xA] = { type = "byte",	name = "controller.%d.plugged" },
+	}
+
+	for port, address in ipairs(controllers) do
+		for offset, info in pairs(controller_struct) do
+			memorymap[address + offset] = {
+				type = info.type,
+				debug = info.debug,
+				name = info.name:format(port),
+			}
+		end
+	end
+end
+
 local abs = math.abs
 
 -- These functions seem to apply to many games
