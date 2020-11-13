68cb75c04edcc36df8eeff98088f08e147cf6fd5
little512 <skidmanxdd@gmail.com>
2020-11-13 00:44:23 -0600

    add support for Super Monkey Ball (PAL)

---
+++ source/modules/games/GMBP8P-0.lua
@@ -0,0 +1,43 @@
+-- Super Monkey Ball (PAL v1.0)
+
+local core = require("games.core")
+
+local game = {
+	memorymap = {}
+}
+
+local addr = 0x80225E00
+local offset = 0x3C
+
+local controllers = {
+	[1] = addr + offset * 0,
+	[2] = addr + offset * 1,
+	[3] = addr + offset * 2,
+	[4] = addr + offset * 3,
+}
+
+local controller_struct = {
+	[0x0] = { type = "short",	name = "controller.%d.buttons.pressed" },
+	[0x2] = { type = "sbyte",	name = "controller.%d.joystick.x" },
+	[0x3] = { type = "sbyte",	name = "controller.%d.joystick.y" },
+	[0x4] = { type = "sbyte",	name = "controller.%d.cstick.x" },
+	[0x5] = { type = "sbyte",	name = "controller.%d.cstick.y" },
+	[0x6] = { type = "byte",	name = "controller.%d.analog.l" },
+	[0x7] = { type = "byte",	name = "controller.%d.analog.r" },
+	[0xA] = { type = "byte",	name = "controller.%d.plugged" },
+}
+
+for port, address in ipairs(controllers) do
+	for offset, info in pairs(controller_struct) do
+		game.memorymap[address + offset] = {
+			type = info.type,
+			debug = info.debug,
+			name = info.name:format(port),
+		}
+	end
+end
+
+game.translateAxis = core.translateAxis
+game.translateTriggers = core.translateTriggers
+
+return game
