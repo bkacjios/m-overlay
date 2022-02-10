eab79a4abf5b79b4d2115d6dd2250d083b090e4b
Bkacjios <blackops7799@gmail.com>
2022-02-10 14:08:05 -0500

    Add support for LOZ: 4 Swords (NTSC-U/J) (#137)

---
+++ source/modules/games/G4SE01-0.lua
@@ -0,0 +1,5 @@
+-- The Legend of Zelda: Four Swords Adventures (NTSC-U v1.0)
+
+local core = require("games.core")
+
+return core.newGame(0x8052EA28)
---
+++ source/modules/games/G4SJ01-0.lua
@@ -0,0 +1,5 @@
+-- The Legend of Zelda: Four Swords Plus (NTSC-J v1.0)
+
+local core = require("games.core")
+
+return core.newGame(0x805256B8)
