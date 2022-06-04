4841e7de693553d1f290561231509aeffe38d3c1
Brad S <bradley.sadd@gmail.com>
2022-06-04 16:30:43 +0100

    Added Scooby Doo! Unmasked

---
+++ source/modules/games/G5DE78-0.lua
@@ -0,0 +1,5 @@
+-- Scooby-Doo! Unmasked (NTSC-U)
+
+local core = require("games.core")
+
+return core.newGame(0x8023380C)
---
+++ source/modules/games/G5DP78-0.lua
@@ -0,0 +1,5 @@
+-- Scooby-Doo! Unmasked (Europe) (En,Fr) (PAL v1.0)
+
+local core = require("games.core")
+
+return core.newGame(0x8023380C)
