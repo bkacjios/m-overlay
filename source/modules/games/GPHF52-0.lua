17ac05cb5c6d3af6632b8a09469abb3be1beb13d
UltiNaruto <zenaruto@ymail.com>
2022-06-15 17:36:37 +0200

    Added support for Pitfall The Lost Expedition FR v1.0 Added support for Sonic Adventure DX PAL v1.1 Added support for Sonic Adventure 2: Battle PAL v1.0

---
+++ source/modules/games/GPHF52-0.lua
@@ -0,0 +1,5 @@
+-- Pitfall The Lost Expedition (FR v1.0)
+
+local core = require("games.core")
+
+return core.newGame(0x80423408)
---
+++ source/modules/games/GXSP8P-1.lua
@@ -0,0 +1,5 @@
+-- Sonic Adventure DX (PAL v1.1)
+
+local core = require("games.core")
+
+return core.newGame(0x80788600)
