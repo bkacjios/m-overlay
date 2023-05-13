4c1b55d2e8396ea3d0db941181dfe61f045432fe
UltiNaruto <zenaruto@ymail.com>
2023-05-13 20:47:38 +0200

    More pitfall games to support Added support for Pitfall The Lost Expedition PAL v1.0 Added support for Pitfall The Lost Expedition DE v1.0 Added support for Pitfall The Big Adventure NTSC-U v1.0 Added support for Pitfall The Big Adventure PAL v1.0

---
+++ source/modules/games/GPHD52-0.lua
@@ -0,0 +1,5 @@
+-- Pitfall The Lost Expedition (DE v1.0)
+
+local core = require("games.core")
+
+return core.newGame(0x80423428)
---
+++ source/modules/games/RPFE52-0.lua
@@ -0,0 +1,5 @@
+-- Pitfall The Big Adventure (NTSC v1.0)
+
+local core = require("games.core")
+
+return core.newGame(0x80456CA0)
