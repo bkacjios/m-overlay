db20a818d05962a924fd3dae314b6486a314a638
PSanth <pratulyansantharam@gmail.com>
2024-02-04 09:29:38 -0500

    Added Sonic Riders and SRTE 2.3

---
+++ source/modules/games/GXEE8P-0.lua
@@ -0,0 +1,5 @@
+-- Sonic Riders (NTSC-U v1.0)
+
+local core = require("games.core")
+
+return core.newGame(0x801AF9C0)
---
+++ source/modules/games/GXSRTE-0.lua
@@ -0,0 +1,5 @@
+-- Sonic Riders Tournament Edition (2.3)
+
+local core = require("games.core")
+
+return core.newGame(0x804B6F50)
